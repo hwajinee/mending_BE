a030398abaec66e77268af418e836583ed278f2a
hwajin <106234345+hwajinee@users.noreply.github.com>
2026-02-27 19:26:51 +0900

    Add files via upload

---
+++ src/main/java/com/example/mending_BE/MendingBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mending_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MendingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MendingBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mending_BE/MendingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mending_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MendingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
